13dc4433e6032a0e0d53bf507de405d9c1fcb837
Klemen Flegar <flegar.klemen@gmail.com>
2025-07-22 16:16:15 +0200

    Lmao

--- backend/yt_dlp_test.py
+++
@@ -1,9 +0,0 @@
-
-from yt_dlp import YoutubeDL
-def download_video(link):
-    ydl_opts = {}
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([link])
-
-link = input("Vnesi YouTube povezavo: ")
-download_video(link)
